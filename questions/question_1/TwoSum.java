ed1175ea9b613017eb66accb434b44a6056bde79
amanraj953426 <amanraj953426@gmail.com>
2025-08-13 02:15:21 +0530

    Solved question 1

--- questions/question_1/TwoSum.java
+++ questions/question_1/TwoSum.java
@@ -14,32 +14,49 @@
 
 package questions.question_1;
 
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 
 public class TwoSum {
+    public static int[] search(int[] arr, int target){
+        // Initialize HashMap
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        // Traversing in array
+        for (int i=0; i<arr.length; i++){
+            int num = target - arr[i]; // find second number
+            if (hm.containsKey(num)){ // containsKey() search num is present in map?
+                return new int[] {hm.get(num),i}; // if yes then return the index
+            }
+            hm.put(arr[i],i); // if not present in the Map then add number and their index in Map
+        }
+        return new int[] {-1,-1}; // if not then return false
+    }
     public static void main(String[] args) {
 
-        ArrayList<Integer> a1 = new ArrayList<>();
-        System.out.println("Enter an Array");
+        // Take input
         Scanner sc = new Scanner(System.in);
 
-        while (true){
-            String str = sc.next();
+        System.out.println("Enter size of an array");
+        int size = sc.nextInt(); // size of array
+        System.out.println("Enter "+size+" array" );
+        int[] arr = new int[size];
+        for (int i=0; i<size; i++){
+            arr[i] = sc.nextInt(); // input an array
+        }
 
-                if (str.equalsIgnoreCase("q")){
-                    break;
-                }
-                try {
-                    int num = Integer.parseInt(str);
-                    a1.add(num);
-                }catch (NumberFormatException e){
-                    System.out.println("Invalid input! Please enter an integer or 'stop'.");
-                }
+        // Print an array
+        for (int j : arr){
+            System.out.print(j+" ");
 
         }
-        for (int i : a1){
-            System.out.println(i);
-        }
+
+        System.out.println();
+        System.out.println("Enter target");
+        int target = sc.nextInt();
+
+        // call search method
+        int[] res = search(arr, target);
+        System.out.println("Index "+res[0]+" "+res[1]);
+
+
     }
 }
